2f5e03e5b471da7ba732f27a366af10c477be340
mathgraf <math.n.graf@gmail.com>
2020-10-28 02:29:59 -0300

    challenge complete

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -10,24 +10,50 @@ app.use(cors());
 
 const repositories = [];
 
+function validateId(request, response, next) {
+  const { id } = request.params
+
+  if (!isUuid(id)) return response.status(400).json({ error: 'ID de repositório inválido.' })
+
+  next()
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const data = {...request.body, id: uuid(), likes: 0}
+
+  repositories.push(data)
+  return response.json(data)
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", validateId, (request, response) => {
+  const { id } = request.params
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+  repositories[repositoryIndex] = {...request.body, id: id, likes: repositories[repositoryIndex].likes}
+
+  return response.json(repositories[repositoryIndex])
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", validateId, (request, response) => {
+  const { id } = request.params
+  
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+  repositories.splice(repositoryIndex, 1)
+
+  return response.status(204).json()
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", validateId, (request, response) => {
+  const { id } = request.params
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+  repositories[repositoryIndex].likes++
+
+  return response.json(repositories[repositoryIndex])
 });
 
 module.exports = app;
